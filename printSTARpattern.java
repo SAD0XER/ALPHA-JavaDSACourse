9122be54bb668c139689d6a34b7ca70abdca5956
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2022-09-05 00:21:09 +0530

    Update the code of Star Pattern make it very interactive.

--- printSTARpattern.java
+++ printSTARpattern.java
@@ -1,8 +1,18 @@
+package temp;
+
+import java.util.*;
+
 public class printSTARpattern {
-	public static void main(String[] sarvesh) {
-		for (int line=1; line<=4; line++) {
+	public static void main(String[] ParaCoder) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter line number: ");
+		int lNum = scan.nextInt();
+
+		//Outer Loop
+		for (int line=1; line<=lNum; line++) {
+			//Inner Loop
 			for (int star = 1; star<=line; star++) {
-				System.out.print("*");
+				System.out.print("* ");
 			}
 			System.out.println();
 		}
