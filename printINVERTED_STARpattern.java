1a27cd4e4fa1001a61e8f10c55d804e0aa71483a
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2022-09-05 22:26:04 +0530

    No Change.

--- printINVERTED_STARpattern.java
+++ printINVERTED_STARpattern.java
@@ -10,6 +10,8 @@ public class printINVERTED_STARpattern {
 		for (int line=1; line<=lNum; line++) {
 			//Inner Loop
 			for (int star = 1; star<=(lNum-line+1); star++) {
+				/* if your star count arts from 0 the you need to write condition like
+				 * for (int star=1; star<=(NumOfLine - Line)) {}*/
 				System.out.print("* ");
 			}
 			System.out.println();
