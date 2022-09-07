4f55c5919e6e005e2cf932c9243bc4b39e0e89cb
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2022-09-07 15:55:22 +0530

    NumberIsPrimeOrNot.java is updated.

--- NumberIsPrimeOrNot.java
+++ NumberIsPrimeOrNot.java
@@ -1,25 +1,49 @@
 import java.util.*;
 
 public class NumberIsPrimeOrNot {
-	public static void main(String[] sadoxer) {
 
+	//function for checking the given number is prime or not.
+	public static boolean isPrime(int n) {
+// your number must be <=2
+		if (n == 2) {
+			return true;
+		} else {
+			for (int i = 2; i <= Math.sqrt(n); i++) {
+				/* for (int i=2; i<=n-1; i++) This is for large numbers,
+				but it is algorithmically right. */
+				if (n % i == 0) { //if it is completely dividing.
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
+	public static void main(String[] sadoxer) {
 		Scanner scan = new Scanner(System.in);
 
-		boolean isPrime = true;
+		/*boolean isPrime = true;
+		System.out.print("Enter Your Number: ");
 		int n = scan.nextInt();
 
 		if (n == 2) {
 			System.out.println(n + " is the Prime Number.");
 		} else {
-			for (int i=2; i<Math.sqrt(n); i++) {
-				if (n % i == 0) isPrime = false;
+			for (int i = 2; i < Math.sqrt(n); i++) {
+				//for (int i=2; i<n-1; i++) This is for small numbers, but it is a very time and space consuming.
+				if (n % i == 0) {
+					isPrime = false;
+					break;
+				}
 			}
 
-		if (isPrime == false) {
-			System.out.println("Number is Not a prime.");
-		} else {
-			System.out.println("Number is Prime.");
-		}
-		}
+			if (!isPrime) {
+				System.out.println("Number is Not a prime.");
+			} else {
+				System.out.println("Number is Prime.");
+			}
+		}*/
+
+		System.out.println(isPrime(2));
 	}
 }
