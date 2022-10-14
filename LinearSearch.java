abe875d53d3d5c692724dd660665bcecea79f876
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2022-10-14 10:25:52 +0530

    Learn and Did the practics of linear search algorithm.

---
+++ LinearSearch.java
@@ -0,0 +1,26 @@
+public class LinearSearch {
+
+	//Linear Search
+	public static int linearSearch(int[] array, int key) {
+		for (int i = 0; i < array.length; i++) {
+			if (key == array[i]) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] sadoxer) {
+		int[] array = {1, 34, 54, 66, 77, 88, 99};
+		int key = 8;
+
+		linearSearch(array, key); //function call
+
+		int index = linearSearch(array, key);
+		if (index == -1) {
+			System.out.println("KEY not found.");
+		} else {
+			System.out.println("Key found at the index: " + index);
+		}
+	}
+}
