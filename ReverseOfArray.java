0fed1a33864e0438fe36ea26eaba940328fc020d
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2022-10-15 16:32:39 +0530

    Understand the logic and right a code for Reverse Of Array.

---
+++ ReverseOfArray.java
@@ -0,0 +1,27 @@
+public class ReverseOfArray {
+
+	public static void reverseArray(int[] array) {
+		int first = 0, last = array.length - 1;
+
+		while (first <= last) {
+
+			// using temporary variable
+			int temp = array[last];
+
+			array[last] = array[first];
+			array[first] = temp;
+			first++; //first = first + 1;
+			last--; //last = last - 1;
+		}
+	}
+
+	public static void main(String[] sadoxer) {
+		int[] array = {1, 2, 3, 4, 5};
+
+		reverseArray(array);
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+	}
+}
