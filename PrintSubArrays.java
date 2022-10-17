b62a6d5f5db3b558423dca861be4decdc8765a44
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2022-10-17 13:20:08 +0530

    Understand and Practice the code of Printing All Sub-Arrays in given array.

---
+++ PrintSubArrays.java
@@ -0,0 +1,24 @@
+public class PrintSubArrays {
+
+	public static void printSubArray(int[] array) {
+		int ts = 0;
+		for (int i = 0; i < array.length; i++) {
+			int start = i;
+			for (int j = i; j < array.length; j++) {
+				int end = j;
+				for (int k = start; k <= end; k++) {
+					System.out.print(array[k] + " ");
+				}
+				ts++;
+				System.out.println();
+			}
+			System.out.println();
+		}
+		System.out.println("Total subarray: " + ts);
+	}
+
+	public static void main(String[] sadoxer) {
+		int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		printSubArray(array);
+	}
+}
