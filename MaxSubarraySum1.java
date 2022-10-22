634115643101e70fb10b03fa8fc7f3a7b7faf7e1
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2022-10-22 15:28:58 +0530

    Correct the code and understand the logic behind it.

--- MaxSubarraySum1.java
+++ MaxSubarraySum1.java
@@ -1,26 +1,33 @@
 //Max Sum of Subarray-1 (Brute Force): Time Complexity O(n³)
-public class MaxSubarraySum {
+public class MaxSubarraySum1 {
 
-	public static void MaxSumSubArray1(int[] array) {
-		int max = Integer.MIN_VALUE, sum = 0;
+	public static void MaxSumSubArray(int[] array) {
+		int max = Integer.MIN_VALUE, currentSum = 0;
 
+		//starting point of array.
 		for (int i = 0; i < array.length; i++) {
-			sum = 0;
+
+			//ending point of array.
 			for (int j = i; j < array.length; j++) {
+				currentSum = 0;
+
+				//sub array
 				for (int k = i; k <= j; k++) {
-					sum += array[k];
-					System.out.print(array[k] + " ");
+					currentSum = currentSum + array[k];
+					System.out.print(array[k] + "  ");
 				}
+				System.out.print(" = " + currentSum);
+				if (max < currentSum) max = currentSum; //comparison
 				System.out.println();
-				if (max < sum) max = sum;
 			}
 			System.out.println();
 		}
-		System.out.println("max = " + max);
+		System.out.println("max sum  = " + max);
 	}
 
 	public static void main(String[] sadoxer) {
-		int[] array = {-3, 4, -1, 7, 5, -8};
-		MaxSumSubArray1(array);
+		int[] array = {1, -2, 6, -1, 3};
+
+		MaxSumSubArray(array);
 	}
 }
