d6ed782220c8c95e55387b7e7d926c251e881387
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2022-11-04 13:21:45 +0530

    Understand Logic and practice the pattern.

---
+++ PrintButterflyPattern.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class PrintButterflyPattern {
+	public static void main(String[] sadoxer) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the Number of Line: ");
+		int line = scan.nextInt();
+
+		for (int i = 1; i <= line; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+
+			for (int j = 1; j <= (line-i)*2; j++) {
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		for (int i = line; i >= 1; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+
+			for (int j = 1; j <= (line-i)*2; j++) {
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
