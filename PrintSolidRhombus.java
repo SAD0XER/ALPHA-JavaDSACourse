188628ab84cd392277721e236a187f026d22a8c4
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2022-11-05 10:47:54 +0530

    Understand the logic and Practice the code.

---
+++ PrintSolidRhombus.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class PrintSolidRhombus {
+	public static void main(String[] sadoxer) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the Number of Line: ");
+		int line = scan.nextInt();
+
+		for (int i = 1; i <= line; i++) {
+			for (int j = 1; j <= (line-i); j++) {
+				System.out.print(" ");
+			}
+
+			for (int k = 1; k <= line; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
