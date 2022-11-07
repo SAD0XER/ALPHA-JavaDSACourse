a0db999dc9a16acc9403a47d3561f11cc61370db
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2022-11-07 19:11:16 +0530

    Understand Logic & Practice the code HollowRhombusPattern

---
+++ PrintHollowRhombusPattern.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PrintHollowRhombusPattern {
+	public static void main(String[] sadoxer) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the Number of Line: ");
+		int line = scan.nextInt();
+
+		for (int i = 1; i <= line; i++) {
+			for (int j = 1; j <= (line-i); j++) {
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= line; j++) {
+				if (i==1 || i==line || j==1 || j==line) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
