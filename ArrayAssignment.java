6079eb4e2ee5ba9abf1ab4db233df1368d48005a
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2022-11-07 19:16:39 +0530

    Format the code lines.

--- ArrayAssignment.java
+++ ArrayAssignment.java
@@ -1,10 +1,9 @@
 public class ArrayAssignment {
-	
-/* Question 1: Given an integer array,
-return true if any value appears at least twice in the array,
-and return false if every element is distinct.
+
+/* Question 1:
 IO:[1, 2, 3, 1], OP:true //case 1
-IO:[1, 2, 3, 4], OP:false //case 2*/
+IO:[1, 2, 3, 4], OP:false //case 2
+IO:[1, 1, 3, 3, 4, 3, 2, 4, 2], OP:true //case 3*/
 
 	public static boolean atLeastTwice(int[] array) {
 		for (int i = 0; i < array.length; i++) {
@@ -17,9 +16,12 @@ IO:[1, 2, 3, 4], OP:false //case 2*/
 		return false;
 	}
 
-	public static void main(String[] sadoxer) {
-		int[] array = {1, 2, 3, 4};
+/**/
 
-		System.out.println(atLeastTwice(array));
+
+	public static void main(String[] sadoxer) {
+		int[] array = {1, 1, 3, 3, 4, 3, 2, 4, 2};
+
+//		System.out.println(atLeastTwice(array));
 	}
 }
