c14862966f313cc2a7ecce54118e7675ba2ff296
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2022-11-18 15:54:31 +0530

    solve the Array Assignment Question3: Max profit by selling & buying stocks

--- ArrayAssignment.java
+++ ArrayAssignment.java
@@ -1,9 +1,11 @@
+import java.math.*;
+
 public class ArrayAssignment {
 
 /* Question 1:
 IO:[1, 2, 3, 1], OP:true //case 1
 IO:[1, 2, 3, 4], OP:false //case 2
-IO:[1, 1, 3, 3, 4, 3, 2, 4, 2], OP:true //case 3*/
+IO:[1, 1, 3, 3, 4, 3, 2, 4, 2], OP:true //case 3 */
 
 	public static boolean atLeastTwice(int[] array) {
 		for (int i = 0; i < array.length; i++) {
@@ -16,12 +18,30 @@ IO:[1, 1, 3, 3, 4, 3, 2, 4, 2], OP:true //case 3*/
 		return false;
 	}
 
-/**/
+/* Question 3:
+ * IO:[7, 1, 5, 3, 6, 4], OP: 5 //case 1
+ * IO:[7, 6, 4, 3, 1], OP: 0 //case2 */
 
+	public static int maxProfitByTradingStocks(int[] array) {
+		int buyingPrice = Integer.MAX_VALUE; // comparing
+		int maxProfit = 0; // storing max profit
+
+		for (int i = 0; i < array.length; i++) {
+			if (buyingPrice < array[i]) {
+				int profit = array[i] - buyingPrice;
+				maxProfit = Math.max(maxProfit, profit);
+			} else {
+				buyingPrice = array[i];
+			}
+		}
+		return maxProfit;
+	}
 
 	public static void main(String[] sadoxer) {
-		int[] array = {1, 1, 3, 3, 4, 3, 2, 4, 2};
-
+//		int[] array = {1, 1, 3, 3, 4, 3, 2, 4, 2};
 //		System.out.println(atLeastTwice(array));
+
+		int[] array = {7, 6, 4, 3, 1};
+		System.out.println(maxProfitByTradingStocks(array));
 	}
 }
