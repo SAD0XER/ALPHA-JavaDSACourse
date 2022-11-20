43af667351c15b5d6ea9278a5298ea8e58d4e851
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2022-11-20 12:08:53 +0530

    Added Optimized solution of Question 4:

--- ArrayAssignment.java
+++ ArrayAssignment.java
@@ -16,9 +16,9 @@ IO:[1, 1, 3, 3, 4, 3, 2, 4, 2], OP:true //case 3 */
 		return false;
 	}
 
-/* Question 3:
- * IO:[7, 1, 5, 3, 6, 4], OP: 5 //case 1
- * IO:[7, 6, 4, 3, 1], OP: 0 //case 2 */
+	/* Question 3:
+	 * IO:[7, 1, 5, 3, 6, 4], OP: 5 //case 1
+	 * IO:[7, 6, 4, 3, 1], OP: 0 //case 2 */
 
 	public static int maxProfitByTradingStocks(int[] array) {
 		int buyingPrice = Integer.MAX_VALUE; // comparing
@@ -35,9 +35,9 @@ IO:[1, 1, 3, 3, 4, 3, 2, 4, 2], OP:true //case 3 */
 		return maxProfit;
 	}
 
-/* Question 4:
-* IO:[0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], OP: 6 //case 1
-* IO:[4, 2, 0, 3, 2, 5], OP: 9 //case 2 */
+	/* Question 4:
+	 * IO:[0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], OP: 6 //case 1
+	 * IO:[4, 2, 0, 3, 2, 5], OP: 9 //case 2 */
 
 	public static int trappedRainWater(int[] array) {
 		int l = array.length;
@@ -47,13 +47,13 @@ IO:[1, 1, 3, 3, 4, 3, 2, 4, 2], OP:true //case 3 */
 		//left max array
 		leftMax[0] = array[0];
 		for (int i = 1; i < l; i++) {
-			leftMax[i] = Math.max(array[i], leftMax[i-1]);
+			leftMax[i] = Math.max(array[i], leftMax[i - 1]);
 		}
 
 		//right max array
-		rightMax[l-1] = array[l-1];
-		for (int i = l-2; i >= 0; i--) {
-			rightMax[i] = Math.max(array[i], rightMax[i+1]);
+		rightMax[l - 1] = array[l - 1];
+		for (int i = l - 2; i >= 0; i--) {
+			rightMax[i] = Math.max(array[i], rightMax[i + 1]);
 		}
 
 		int trappedWater = 0;
@@ -62,7 +62,27 @@ IO:[1, 1, 3, 3, 4, 3, 2, 4, 2], OP:true //case 3 */
 			trappedWater += waterLevel - array[i]; //calculate trapped water
 		}
 		return trappedWater;
+
+		/*//New Very optimized solution on this problem.
+		int l = array.length;
+
+		int trappedWater = 0, left = 0, right = l - 1;
+		int[] rMax = new int[right], lMax = new int[left];
+
+		while (left < right) {
+			if (lMax < rMax) {
+				left++;
+				lMax = Math.max(lMax, array[left]);
+				trappedWater += lMax - array[left];
+			} else {
+				right--;
+				rMax = Math.max(rMax, array[right]);
+				trappedWater += rMax - array[right];
+			}
+		}
+		return trappedWater;*/
 	}
+
 
 	public static void main(String[] sadoxer) {
 //		int[] array = {1, 1, 3, 3, 4, 3, 2, 4, 2};
@@ -71,7 +91,10 @@ IO:[1, 1, 3, 3, 4, 3, 2, 4, 2], OP:true //case 3 */
 //		int[] array = {7, 6, 4, 3, 1};
 //		System.out.println(maxProfitByTradingStocks(array));
 
-		int[] array = {4, 2, 0, 3, 2, 5};
-		System.out.println(trappedRainWater(array));
+//		int[] array = {4, 2, 0, 3, 2, 5};
+//		System.out.println(trappedRainWater(array));
+
+		int[] array = {};
+		System.out.println((array));
 	}
 }
