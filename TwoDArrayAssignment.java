57cabe5c3bff04296ad8cb4897fbae623599e63c
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2022-12-15 13:49:26 +0530

    Solved all assignment Questions.

--- TwoDArrayAssignment.java
+++ TwoDArrayAssignment.java
@@ -24,27 +24,39 @@ public class TwoDArrayAssignment {
         return sum;
     }
 
+    //Question 3: Write a program to Find Transpose of a Matrix.
+    public static void Q3(int[][] matrix) {
+        int[][] mat = new int[matrix[0].length][matrix.length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i == j) {
+                    mat[i][j] = matrix[i][j];
+                } else {
+                    mat[j][i] = matrix[i][j];
+                }
+            }
+        }
+        //Printing Transposed Array.
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                System.out.print(mat[i][j]);
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] para_coder) {
 //        Question 1:
 //        int[][] array = {{4,7,8}, {8,8,7}}; int count = 0;
 //        System.out.println(Q1(array, 7));
-        /*for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[0].length; j++) {
-                if (8 == array[i][j]) {
-                    count++;
-                }
-            }
-        }
-        System.out.println("This number arrived " + count + " times.");*/
 
 //        Question 2:
-        int[][] array = {{1,4,9}, {11,4,3}, {2,2,3}};
+//        int[][] array = {{1,4,9}, {11,4,3}, {2,2,3}};
 //        System.out.println(Q2(array, 2));
-        int sum = 0;
-        for (int i = 0; i < array[1].length; i++) {
-            sum += array[1][i];
-        }
-        System.out.println("The sum of Row is " + sum);
+
+//        Question 3:
+//        int[][] array = {{1, 2, 3}, {4, 5, 6}};
+//        Q3(array);
     }
 }
