1675e52f1ea6d342f19bfd7f34148c6105d3f95e
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-01-21 14:00:10 +0530

    Practice Fibo & isSorted Questions & Understand the call stack of their recursion.

--- Recursion.java
+++ Recursion.java
@@ -5,7 +5,7 @@ public class Recursion {
             return;
         }
         System.out.print(N + "\t");
-        printDec(N-1);
+        printDec(N - 1);
     }
 
     public static void printInc(int N) {
@@ -18,21 +18,32 @@ public class Recursion {
     }
 
     public static int factorial(int N) {
-        if (N == 0) {
-            return 1;
-        }
+        if (N == 0) return 1;
         int fnm1 = factorial(N - 1);
         int fn = N * fnm1;
         return fn;
     }
 
     public static int sumNatural(int N) {
-        if (N == 1) {
-            return 1;
-        }
+        if (N == 1) return 1;
+
         int snm1 = sumNatural(N - 1);
         int son = N + snm1;
         return son;
+    }
+
+    public static int fibonacci(int N) {
+        if (N == 0 || N == 1) return N;
+        int fnm1 = fibonacci(N - 1);
+        int fnm2 = fibonacci(N - 2);
+        int fn = fnm1 + fnm2;
+        return fn;
+    }
+
+    public static boolean isSorted(int[] arr, int i) {
+        if (i == arr.length - 1) return true;
+        if (arr[i] > arr[i + 1]) return false;
+        return isSorted(arr, i+1);
     }
 
     public static void main(String[] para_coder) {
@@ -40,5 +51,8 @@ public class Recursion {
 //        printInc(10);
 //        System.out.println(factorial(5));
 //        System.out.println(sumNatural(5));
+//        System.out.println(fibonacci(5));
+/*        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(isSorted(arr, 0));*/
     }
 }
