49d7cdd7e994636a52dfad16fb608386a32ab675
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-01-30 19:18:24 +0530

    correct typo error.

--- Strings.java
+++ Strings.java
@@ -137,7 +137,7 @@ public class Strings {
         }
     }
 
-    public static void main(String[] para_coder) s{
+    public static void main(String[] para_coder) {
         Scanner scan = new Scanner(System.in);
 
         //String declartion.
