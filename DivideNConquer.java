b0b6eab12f8b3c78706a8e276c89203bd69349c8
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-01-31 13:32:59 +0530

    Understand the code & write, practice the code of MergeSort Algorithm.

--- DivideNConquer.java
+++ DivideNConquer.java
@@ -7,9 +7,10 @@ public class DivideNConquer {
     }
 
     public static void mergeSort(int[] arr, int si, int ei) {
-        //Base Case
+        //base case
         if (si >= ei) return;
-        int mid = si + (ei - si) / 2; //or mid = (si + ei)/2;
+
+        int mid = si + (ei - si) / 2;
         mergeSort(arr, si, mid);
         mergeSort(arr, mid + 1, ei);
 
@@ -18,10 +19,11 @@ public class DivideNConquer {
 
     public static void merge(int[] arr, int si, int mid, int ei) {
         int[] temp = new int[ei - si + 1];
-        int i = si; //idx for 1st sorted part/left
-        int j = mid + 1; //idx for 2nd sorted part/right
+        int i = si; //idx for left array
+        int j = mid + 1; //idx for right array
         int k = 0; //idx for temp array
 
+        //comparing left arr with right arr & assigning to that respective page.
         while (i <= mid && j <= ei) {
             if (arr[i] < arr[j]) {
                 temp[k] = arr[i];
@@ -33,24 +35,25 @@ public class DivideNConquer {
             k++;
         }
 
-        //for leftover elements of 1st sorted part
+        //Here we're pushing those sorted array into temporary array.
+        //Left side of temporary array.
         while (i <= mid) {
             temp[k++] = arr[i++];
         }
 
-        //for rightover elements of 2nd sorted part
+        //Left side of temporary array.
         while (j <= ei) {
             temp[k++] = arr[j++];
         }
 
-        //copy temp to original array
+        //copying temp array into main array.
         for (k = 0, i = si; k < temp.length; k++, i++) {
             arr[i] = temp[k];
         }
     }
 
     public static void main(String[] para_coder) {
-        int[] arr = {6, 3, 9, 5, 2, 8};
+        int[] arr = {6, 3, 9, 5, 2, 8, -2};
         mergeSort(arr, 0, arr.length - 1);
         printArray(arr);
     }
