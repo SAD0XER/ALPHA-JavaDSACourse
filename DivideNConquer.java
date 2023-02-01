3387fe52e2cffa9cf2ca5d5575e68b6ce72df1ad
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-02-01 15:54:44 +0530

    Learn Quick Sort Algorithm & write the code.

--- DivideNConquer.java
+++ DivideNConquer.java
@@ -52,9 +52,41 @@ public class DivideNConquer {
         }
     }
 
+    public static void quickSort(int arr[], int si, int ei) {
+        if (si >= ei) return;
+
+        int pIdx = partition(arr, si, ei);
+
+        quickSort(arr, si, pIdx - 1); //left
+        quickSort(arr, pIdx + 1, ei); //right
+    }
+
+    public static int partition(int[] arr, int si, int ei) {
+        int pivot = arr[ei];
+        int i = si - 1;
+
+        for (int j = si; j < ei; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                //swapping
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        //
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i]; //Don't write `pivot = arr[i];`
+        arr[i] = temp;
+        return i;
+    }
+
     public static void main(String[] para_coder) {
         int[] arr = {6, 3, 9, 5, 2, 8, -2};
-        mergeSort(arr, 0, arr.length - 1);
+//        mergeSort(arr, 0, arr.length - 1);
+//        printArray(arr);
+        quickSort(arr, 0, arr.length - 1);
         printArray(arr);
     }
 }
