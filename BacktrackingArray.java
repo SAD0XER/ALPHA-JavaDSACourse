324792de45be307befe54d770782d11051959bc6
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-06-11 18:19:58 +0530

    Check if problem can be solved & print only 1 solution to N-Queen problem.

--- BacktrackingArray.java
+++ BacktrackingArray.java
@@ -44,25 +44,28 @@ public class BacktrackingArray {
         }
     }
 
-    public static void nQueens(char[][] board, int row) {
+    public static boolean nQueens(char[][] board, int row) {
         //base case
         if (row == board.length) {
 //            printBoard(board);
-            count++;
-            return;
+//            count++;
+            return true;
         }
 
         //recursion for col
         for (int j = 0; j < board.length; j++) {
             if (isSafe(board, row, j)) {
                 board[row][j] = 'Q';
-                nQueens(board, row + 1); //recursive call
-                board[row][j] = 'x'; //backtracking step
+                if (nQueens(board, row + 1)){
+                    return true;
+                }
+//                board[row][j] = 'x'; //backtracking step
             }
         }
+        return false;
     }
 
-    static int count = 0;
+//    static int count = 0;
 
     public static boolean isSafe(char[][] board, int row, int col) {
         //up side
@@ -88,15 +91,15 @@ public class BacktrackingArray {
         return true;
     }
 
-//    public static void printBoard(char[][] board) {
-//        System.out.println("--------Safe Chess Board--------");
-//        for (int i = 0; i < board.length; i++) {
-//            for (int j = 0; j < board.length; j++) {
-//                System.out.print(board[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-//    }
+    public static void printBoard(char[][] board) {
+        System.out.println("--------Safe Chess Board--------");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
     public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
@@ -120,7 +123,13 @@ public class BacktrackingArray {
                 board[i][j] = 'x';
             }
         }
-        nQueens(board, 0);
-        System.out.println("Total ways to solve N-Queen problem is " + count);
+        //Print 1 solution.
+        if (nQueens(board, 0)) {
+            System.out.println("Solution is possible.");
+            printBoard(board);
+        } else {
+            System.out.println("Solution is NOT possible.");
+        }
+//        System.out.println("Total ways to solve N-Queen problem is " + count); //count ways
     }
 }
