3eaf9aef40d0b866462b03b165dc02b550abcbbc
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-06-20 21:38:32 +0530

    Print Reverse of ArrayList

--- Array_List.java
+++ Array_List.java
@@ -1,9 +1,9 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Array_List {
     public static void main(String[] para_coder) {
         //Defining all the ArrayList variable
-        ArrayList<Byte> list1 = new ArrayList<>();
+        /*ArrayList<Byte> list1 = new ArrayList<>();
         ArrayList<Short> list2 = new ArrayList<>();
         ArrayList<Integer> list3 = new ArrayList<>();
         ArrayList<Long> list4 = new ArrayList<>();
@@ -11,10 +11,10 @@ public class Array_List {
         ArrayList<Double> list6 = new ArrayList<>();
         ArrayList<Character> list7 = new ArrayList<>();
         ArrayList<String> list8 = new ArrayList<>();
-        ArrayList<Boolean> list9 = new ArrayList<>();
+        ArrayList<Boolean> list9 = new ArrayList<>();*/
 
         //Adding elements in ArrayList.
-        list1.add((byte) 123); list1.add((byte) 1); list1.add((byte) 0);
+        /*list1.add((byte) 123); list1.add((byte) 1); list1.add((byte) 0);
         list2.add((short) 1212); list2.add((short) 3332); list2.add((short) 4443);
         list3.add(1234567); list3.add(3423452); list3.add(3495736);
         list4.add(10000000067L); list4.add(1034L); list4.add(10000430000L);
@@ -22,9 +22,9 @@ public class Array_List {
         list6.add(1.34534544); list6.add(2.34534); list6.add(3.3434554);
         list7.add('a'); list7.add('7'); list7.add('%');
         list8.add("sad"); list8.add("234AD"); list8.add("sdf45$%");
-        list9.add(true); list9.add(true); list9.add(false);
+        list9.add(true); list9.add(true); list9.add(false);*/
 
-        //Getting elements of ArrayList
+        //Getting elements of ArrayList.
         /*System.out.println(list1.get(1));
         System.out.println(list2.get(1));
         System.out.println(list3.get(1));
@@ -35,7 +35,7 @@ public class Array_List {
         System.out.println(list8.get(1));
         System.out.println(list9.get(1));*/
 
-        //Removing elements of ArrayList
+        //Removing elements of ArrayList.
         /*list1.remove(1);
         list2.remove(1);
         list3.remove(1);
@@ -46,7 +46,7 @@ public class Array_List {
         list8.remove(1);
         list9.remove(1);*/
 
-        //Setting elements of ArrayList
+        //Setting elements of ArrayList.
         /*list1.set(1, (byte) 111);
         list2.set(1, (short) 111);
         list3.set(1, 9999);
@@ -57,7 +57,7 @@ public class Array_List {
         list8.set(1, "OM");
         list9.set(1, false);*/
 
-        //Checking ArrayList contains specific value or not
+        //Checking ArrayList contains specific value or not.
         /*System.out.println(list1.contains((byte) 123));
         System.out.println(list2.contains((short) 123));
         System.out.println(list3.contains(1234567));
@@ -68,7 +68,7 @@ public class Array_List {
         System.out.println(list8.contains("sad"));
         System.out.println(list9.contains(false));*/
 
-        //Adding elements at specified index
+        //Adding elements at specified index.
         /*list1.add(2, (byte) 101);
         list2.add(2, (short) 101);
         list3.add(3, 9999);
@@ -78,5 +78,17 @@ public class Array_List {
         list7.add(0, '!');
         list8.add(3, "Aa!1");
         list9.add(3, true);*/
+
+        //Print reverse of ArrayList. O(n)
+        ArrayList<Integer> list = new ArrayList<>();
+        //Adding elements in ArrayList using loop.
+        for (int i = 1; i < 6; i++) {
+            list.add(i);
+        }
+        //Reversing ArrayList.
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
    }
 }
