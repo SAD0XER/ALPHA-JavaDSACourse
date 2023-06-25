6e7b2bb657e180866ad07e4b7c2a6b093c4dfaf7
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-06-25 21:34:54 +0530

    Pair Sum 1: 2 Pointer Approach O(n)

--- Array_List.java
+++ Array_List.java
@@ -47,12 +47,30 @@ public class Array_List {
         return maxWater;
     }*/
 
-    public static boolean pairSum1(ArrayList<Integer> list, int target) {
+    /*public static boolean pairSum1(ArrayList<Integer> list, int target) {
         for (int i = 0; i < list.size(); i++) {
             for (int j = i + 1; j < list.size(); j++) {
                 if (list.get(i) + list.get(j) == target) {
                     return true;
                 }
+            }
+        }
+        return false;
+    }*/
+
+    public static boolean pairSum2(ArrayList<Integer> list, int target) {
+        int lp = 0, rp = list.size() - 1;
+
+        while (lp != rp) {
+            //case 1:
+            if (list.get(lp) + list.get(rp) == target) {
+                return true;
+            }
+            //case 2:
+            if (list.get(lp) + list.get(rp) < target) {
+                lp++;
+            } else { //case 3:
+                rp--;
             }
         }
         return false;
@@ -216,12 +234,12 @@ public class Array_List {
 
         System.out.println(storeWater(height));*/
 
-        //Pair Sum 1: Brute Force Approach O(n^2)
+        //Pair Sum 1: 2 Pointer Approach O(n)
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 1; i <= 6; i++) {
             list.add(i);
         }
-        int target = 5;
-        System.out.println(pairSum1(list, target));
+        int target = 11;
+        System.out.println(pairSum2(list, target));
     }
 }
