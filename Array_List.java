b72eb78f0618d3e6601c9799c9578ea5fdb84ed5
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-06-26 21:40:44 +0530

    Pair Sum 2: 2 Pointer Approach on Sorted & Rotated ArrayList O(n)

--- Array_List.java
+++ Array_List.java
@@ -60,7 +60,7 @@ public class Array_List {
     }*/
 
     //2 Pointer Approach Function.
-    public static boolean pairSum1(ArrayList<Integer> list, int target) {
+    /*public static boolean pairSum1(ArrayList<Integer> list, int target) {
         int lp = 0, rp = list.size() - 1;
 
         while (lp != rp) {
@@ -73,6 +73,35 @@ public class Array_List {
                 lp++;
             } else { //case 3:
                 rp--;
+            }
+        }
+        return false;
+    }*/
+
+    //2 Pointer Approach Function.
+    public static boolean pairSum2(ArrayList<Integer> list, int target) {
+        int n = list.size(), bp = -1; //Breaking Point
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > list.get(i + 1)) {
+                bp = i;
+                break;
+            }
+        }
+
+        int lp = bp + 1; //Smallest Element
+        int rp = bp; //Largest Element
+
+        while (lp != rp) {
+            //Case 1:
+            if (list.get(lp) + list.get(rp) == target) {
+                return true;
+            }
+
+            //Case 2:
+            if (list.get(lp) + list.get(rp) < target) {
+                lp = (lp + 1) % list.size(); //Important Formula
+            } else { //Case 3:
+                rp = (list.size() + rp - 1) % list.size(); //Important Formula
             }
         }
         return false;
@@ -236,12 +265,11 @@ public class Array_List {
 
         System.out.println(storeWater(height));*/
 
-        //Pair Sum 1: 2 Pointer Approach O(n)
+        //Pair Sum 2: 2 Pointer Approach O(n)
         ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 1; i <= 6; i++) {
-            list.add(i);
-        }
-        int target = 11;
-        System.out.println(pairSum1(list, target));
+        list.add(11); list.add(15); list.add(6); list.add(8); list.add(9); list.add(10);
+        int target = 16;
+
+        System.out.println(pairSum2(list, target));
     }
 }
