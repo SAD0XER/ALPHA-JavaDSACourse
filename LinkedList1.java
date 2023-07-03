985d706730659bf3578ee50ad9f4cc7fdcfd4996
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-07-03 20:45:34 +0530

    Created single Node in Linked List

---
+++ LinkedList1.java
@@ -0,0 +1,17 @@
+public class LinkedList1 {
+    //Created single node in LL.
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public static void main(String[] para_coder) {}
+}
