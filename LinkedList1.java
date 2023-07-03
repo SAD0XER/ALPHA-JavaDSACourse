a8809e523669c9e61032f63821e40de0a3af0b8c
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-07-03 21:41:24 +0530

    Created method to Add Node in the Last place of LL.

--- LinkedList1.java
+++ LinkedList1.java
@@ -32,9 +32,28 @@ public class LinkedList1 {
         head = newNode;
     }
 
+    //Adding Node at Last Place.
+    public void addLast(int data) {
+        //step 1: Create New Node.
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        //Step 2: next of tail = newNode (Linking Process)
+        tail.next = newNode;
+
+        //Step 3: Make tail = newNode
+        tail = newNode;
+    }
+
     public static void main(String[] para_coder) {
         LinkedList1 ll = new LinkedList1();
-        ll.addFirst(1);
         ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addFirst(3);
+        ll.addFirst(4);
     }
 }
