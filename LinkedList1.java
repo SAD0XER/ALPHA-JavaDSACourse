acb9a70b45865cc4b6a32aa74759cdeb73843995
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-07-03 21:31:47 +0530

    Created method to Add Node at First place of LL.

--- LinkedList1.java
+++ LinkedList1.java
@@ -13,5 +13,28 @@ public class LinkedList1 {
     public static Node head;
     public static Node tail;
 
-    public static void main(String[] para_coder) {}
+//    Methods of Linked List:
+
+    //Adding Node at first place.
+    public void addFirst(int data) {
+        //Step 1: Create New Node.
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        //Step 2: newNode next = head (Linking Process)
+        newNode.next = head; //link
+
+        //Step 3: head = newNode.
+        head = newNode;
+    }
+
+    public static void main(String[] para_coder) {
+        LinkedList1 ll = new LinkedList1();
+        ll.addFirst(1);
+        ll.addFirst(2);
+    }
 }
