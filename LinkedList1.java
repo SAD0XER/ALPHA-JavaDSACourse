424bf05025d0515cf8adc6d4ed7100ac4988d3b9
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-07-05 20:09:08 +0530

    Created method to Remove First Node of LL.

--- LinkedList1.java
+++ LinkedList1.java
@@ -52,21 +52,6 @@ public class LinkedList1 {
         tail = newNode;
     }
 
-    //Printing the LL.
-    public void printLL() {
-        if (head == null) {
-            System.out.println("LL is Empty.");
-            return;
-        } //Removing this case will result in printing "Null" when the LL is empty.
-
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + "->");
-            temp = temp.next;
-        }
-        System.out.println("Null");
-    }
-
     //Adding in Middle of LL that is at given index.
     public void add(int idx, int data) {
         if (idx == 0) {
@@ -88,14 +73,46 @@ public class LinkedList1 {
         temp.next = newNode;
     }
 
+    public int removeFirst() {
+        //Special cases
+        if (size == 0) {
+            System.out.println("LL is Empty.");
+            return Integer.MIN_VALUE; //Returning Infinity value.
+        } else if (size == 1) {
+            int value = head.data;
+            head = tail = null;
+            size = 0;
+            return value;
+        }
+
+        int value = head.data;
+        head = head.next;
+        size--;
+        return value;
+    }
+
+    //Printing the LL.
+    public void printLL() {
+        if (head == null) {
+            System.out.println("LL is Empty.");
+            return;
+        } //Removing this case will result in printing "Null" when the LL is empty.
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+
     public static void main(String[] para_coder) {
         LinkedList1 ll = new LinkedList1();
-        ll.addFirst(2);
-        ll.addFirst(1);
-        ll.addLast(4);
-        ll.addLast(5);
-        ll.add(2, 3);
+        ll.addFirst(2); ll.addFirst(1); ll.addLast(4); ll.addLast(5); ll.add(2, 3);
+
         ll.printLL(); //1->2->3->4->5->null
-        System.out.println(size);
+//        System.out.println(size);
+        ll.removeFirst();
+        ll.printLL();
     }
 }
