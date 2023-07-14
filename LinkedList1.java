af80595845a266a304715963e0355c257803a448
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-07-14 20:59:04 +0530

    Merge Sort on LL. O(nLog n)

--- LinkedList1.java
+++ LinkedList1.java
@@ -314,6 +314,65 @@ public class LinkedList1 {
         System.out.println("Null");
     }
 
+    //Merge Sort on LL O(nLog n)
+    public Node mergeSort(Node head) {
+        //Base Case
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        //Find Mid
+        Node mid = getMid(head);
+
+        //Call MergeSort for Left Half & Right Half
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+
+        //Merge
+        return merge(newLeft, newRight);
+    }
+
+    private Node getMid(Node head) {
+        Node slow = head, fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next; //+1
+            fast = fast.next.next; //+2
+        }
+        return slow; //Mid Node
+    }
+
+    private Node merge(Node head1, Node head2) {
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+        while (head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+    }
+
     public static void main(String[] para_coder) {
 //        LinkedList1 ll = new LinkedList1();
 //        ll.addFirst(1);
@@ -348,19 +407,32 @@ public class LinkedList1 {
 //        System.out.println(isCycle());
 
         //Create LL
-        LinkedList<Integer> ll = new LinkedList<>();
+//        LinkedList<Integer> ll = new LinkedList<>();
+//
+//        //Add into LL
+//        ll.addLast(1);
+//        ll.addLast(2);
+//        ll.addFirst(0); //0->1->2
+//
+//        System.out.println(ll); //Printing the LL
+//
+//        //Remove from LL
+//        ll.removeLast();
+//        ll.removeFirst();
+//
+//        System.out.println(ll); //Printing the LL
 
-        //Add into LL
-        ll.addLast(1);
-        ll.addLast(2);
-        ll.addFirst(0); //0->1->2
+        //Creating LL
+        LinkedList1 ll = new LinkedList1();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        //5->4->3->2->1->Null
 
-        System.out.println(ll); //Printing the LL
-
-        //Remove from LL
-        ll.removeLast();
-        ll.removeFirst();
-
-        System.out.println(ll); //Printing the LL
+        ll.printLL();
+        head = ll.mergeSort(head);
+        ll.printLL();
     }
 }
