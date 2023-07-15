e606b477edaabf812501e20a7bf0550e137c8ffc
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-07-15 23:13:01 +0530

    Created single Node in Double Linked List.

---
+++ DoubleLinkedList.java
@@ -0,0 +1,21 @@
+public class DoubleLinkedList {
+    //Create Single Node in DLL.
+    public class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+
+    public static void main(String[] para_coder) {}
+}
