cc76ab585636c29c8f86ea1f43c8608c3a777119
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-07-29 18:41:51 +0530

    Did the Implementation of stack usin LinkedList.

--- Stack.java
+++ Stack.java
@@ -34,8 +34,58 @@ public class Stack {
         }
     }
 
+    //Implementation of Stack using Linked List.
+    static class Node {
+        int data;
+        Node next;
+
+        //Constructor.
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class StackLL {
+        static Node head = null;
+
+        //isEmpty(): To know Linked List is Empty or Not.
+        public static boolean isEmpty() {
+            return head == null;
+        }
+
+        //Push(): To Add elements in Linked List.
+        public static void push(int data) {
+            Node newNode = new Node(data);
+
+            if (isEmpty()) {
+                head = newNode;
+                return;
+            }
+
+            newNode.next = head;
+            head = newNode;
+        }
+
+        //Pop(): To Remove element from Linked List.
+        public static int pop() {
+            if (isEmpty()) return -1;
+
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        //Peek(): To see which elements on the top.
+        public static int peek() {
+            if (isEmpty())  return -1;
+
+            return head.data;
+        }
+    }
+
     public static void main(String[] para_coder) {
-        StackAL stack = new StackAL();
+        StackLL stack = new StackLL();
         stack.push(1);
         stack.push(2);
         stack.push(3);
