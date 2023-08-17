c19b582abb2e15292cfae1f478ee401a2a710dfa
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-08-17 18:51:10 +0530

    Circular Queue using Array: Implementation with O(1) Time Complexity of remove() method.

--- Queues.java
+++ Queues.java
@@ -3,44 +3,55 @@ public class Queues {
         static int[] arr;
         static int size;
         static int rear;
+        static int front;
 
         //Constructor
         QueueArr(int n) {
             arr = new int[n];
             size = n;
             rear = -1;
+            front = -1;
         }
 
-        //isEmpty Method.
+        //isEmpty Method: To check queue is Empty or NOT.
         public static boolean isEmpty() {
-            return rear == -1;
+            return rear == -1 && front == -1;
+        }
+
+        //isFull Method: To check Circular Queue is Full or NOT.
+        public static boolean isFull() {
+            return (rear + 1) % size == front;
         }
 
         //Add (Enque) Method. O(1)
         public static void add(int data) {
-            if (rear == size - 1) {
+            if (isFull()) {
                 System.out.println("Queue is Full.");
                 return;
             }
-
-            rear = rear + 1;
+            //Case: while Adding First element.
+            if (front == -1) {
+                front = 0;
+            }
+            rear = (rear + 1) % size;
             arr[rear] = data;
         }
 
-        //Remove (Dequeue) Method. O(n)
+        //Remove (Dequeue) Method. O(1)
         public static int remove() {
             if (isEmpty()) {
                 System.out.print("Queue is Empty.");
                 return -1;
             }
+            int result = arr[front];
 
-            int front = arr[0];
-            for (int i = 0; i < rear; i++) {
-                arr[i] = arr[i + 1];
+            //Case: when Removing Last element.
+            if (rear == front) {
+                rear = front = -1;
+            } else {
+                front = (front + 1) % size;
             }
-
-            rear = rear - 1;
-            return front;
+            return result;
         }
 
         //Peek O(1)
@@ -49,16 +60,18 @@ public class Queues {
                 System.out.print("Queue is Empty.");
                 return -1;
             }
-            return arr[0];
+            return arr[front];
         }
     }
 
     public static void main(String[] para_coder) {
-        QueueArr q = new QueueArr(5); //Here we've defined size of the array.
+        QueueArr q = new QueueArr(3); //Here we've defined size of the array.
         q.add(1);
         q.add(2);
         q.add(3);
+        System.out.println(q.remove());
         q.add(4);
+        System.out.println(q.remove());
         q.add(5);
 
         while (!q.isEmpty()) {
