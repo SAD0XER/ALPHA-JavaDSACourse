311dfc43a58d186eb2cda7af96bc4d112778f195
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-08-23 21:43:45 +0530

    Implementation of Queue using 2 Stacks. Method 1: Add() O(n)

--- Queues.java
+++ Queues.java
@@ -123,6 +123,46 @@ public class Queues {
         }
     }
 
+    static Stack<Integer> s1 = new Stack<>();
+    static Stack<Integer> s2 = new Stack<>();
+
+    //isEmpty
+    public static boolean isEmpty() {
+        return s1.isEmpty();
+    }
+
+    //Add: O(n)
+    public static void add(int data) {
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+
+        s1.push(data);
+
+        while (!s2.isEmpty()) {
+            s1.push(s2.pop());
+        }
+    }
+
+    //Remove: O(1)
+    public static int remove() {
+        if (isEmpty()) {
+            System.out.println("Queue is Empty.");
+            return -1;
+        }
+
+        return s1.pop();
+    }
+
+    //Peek: O(1)
+    public static int peek() {
+        if (isEmpty()) {
+            System.out.println("Queue is Empty.");
+            return -1;
+        }
+        return s1.peek();
+    }
+
     public static void main(String[] para_coder) {
 //        QueueLL q = new QueueLL(); //Here we've defined size of the array.
 
@@ -130,10 +170,11 @@ public class Queues {
          * There are two classes in Java who can implement Queue interfaces. One is Linked List and Second is ArrayDeque.*/
 
 //        Queue<Character> q = new LinkedList<>();
-        Queue<Character> q = new ArrayDeque<>();
-        q.add('a');
-        q.add('b');
-        q.add('c');
+//        Queue<Character> q = new ArrayDeque<>();
+        Queues q = new Queues();
+        q.add(1);
+        q.add(2);
+        q.add(3);
 
         while (!q.isEmpty()) {
             System.out.println(q.peek());
