57d235a57094115617e709c4d445660fb7392e65
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-08-25 21:40:20 +0530

    Implementation of Stack using 2 Queues. Method 2: Add() O(1)

--- Queues.java
+++ Queues.java
@@ -163,6 +163,77 @@ public class Queues {
         return s1.peek();
     }
 
+    static class Stacks {
+        static Queue<Integer> q1 = new LinkedList<>();
+        static Queue<Integer> q2 = new LinkedList<>();
+
+        //isEmpty
+        public static boolean isEmpty() {
+            return q1.isEmpty() && q2.isEmpty();
+        }
+
+        //Push: O(1)
+        public static void push(int data) {
+            if (!q1.isEmpty()) {
+                q1.add(data);
+            } else {
+                q2.add(data);
+            }
+        }
+
+        //Pop: O(n)
+        public static int pop() {
+            if (isEmpty()){
+                System.out.println("Stack is Empty.");
+                return -1;
+            }
+
+            int top = -1;
+
+            if (!q1.isEmpty()){ //Case 1
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    if (q1.isEmpty()) {
+                        break;
+                    }
+                    q2.add(top);
+                }
+            } else { //Case 2
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    if (q2.isEmpty()) {
+                        break;
+                    }
+                    q1.add(top);
+                }
+            }
+            return top;
+        }
+
+        //Peek: O(n)
+        public static int peek() {
+            if (isEmpty()){
+                System.out.println("Stack is Empty.");
+                return -1;
+            }
+
+            int top = -1;
+
+            if (!q1.isEmpty()){ //Case 1
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    q2.add(top);
+                }
+            } else { //Case 2
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    q1.add(top);
+                }
+            }
+            return top;
+        }
+    }
+
     public static void main(String[] para_coder) {
 //        QueueLL q = new QueueLL(); //Here we've defined size of the array.
 
@@ -171,14 +242,15 @@ public class Queues {
 
 //        Queue<Character> q = new LinkedList<>();
 //        Queue<Character> q = new ArrayDeque<>();
-        Queues q = new Queues();
-        q.add(1);
-        q.add(2);
-        q.add(3);
+//        Queues q = new Queues();
+        Stacks s = new Stacks();
+        s.push(1);
+        s.push(2);
+        s.push(3);
 
-        while (!q.isEmpty()) {
-            System.out.println(q.peek());
-            q.remove();
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
         }
     }
 }
