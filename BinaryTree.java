a38bcafd49a70cbcd9f073d0729f0dda50282ba2
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-09-04 23:20:42 +0530

    Created Binary Tree: Build a Binary Tree using Preorder Sequence. O(n)

---
+++ BinaryTree.java
@@ -0,0 +1,39 @@
+public class BinaryTree {
+
+    //Node Class for Binary Tree.
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static int idx = -1;
+
+    //Time Complexity: O(n)
+    public static Node buildTree(int[] nodes) {
+        idx++;
+        if (nodes[idx] == -1) {
+            return null;
+        }
+
+        Node newNode = new Node(nodes[idx]);
+        newNode.left = buildTree(nodes);
+        newNode.right = buildTree(nodes);
+
+        return newNode;
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+
+//        BinaryTree tree = new BinaryTree();
+        Node root = buildTree(nodes);
+        System.out.println(root.data);
+    }
+}
