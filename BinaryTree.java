0bba2f4d083b9f7f47c6106e9cda831fe6b5f36d
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-09-05 16:15:54 +0530

    Tree Traversal using Recursion: #1. Preorder Traversal. O(n)

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,5 +1,9 @@
 public class BinaryTree {
 
+    public class Asd {
+        int Mydata = 23;
+    }
+
     //Node Class for Binary Tree.
     static class Node {
         int data;
@@ -29,11 +33,30 @@ public class BinaryTree {
         return newNode;
     }
 
+    //Tree Traversal.
+    /*#1. Preorder Traversal. Linear Time Complexity: O(n)
+    - There are 3 Rules of Preorder Traversal using Recursion:
+    1st Print the Root.
+    2nd Print Left subtree.
+    3rd Print Right subtree.*/
+    public static void preorder(Node root) { //Taking root of tree as a parameter.
+        if (root == null) { //Base Case
+            System.out.print(-1 + " "); //Add this line if you want to print -1 to show NULL node as well.
+            return;
+        }
+
+        System.out.print(root.data + " "); //Printing Data.
+        preorder(root.left); //Printing Left subtree.
+        preorder(root.right); //Printing Right subtree.
+    }
+
     public static void main(String[] args) {
         int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
 
 //        BinaryTree tree = new BinaryTree();
         Node root = buildTree(nodes);
-        System.out.println(root.data);
+//        System.out.println(root.data);
+
+        preorder(root);
     }
 }
