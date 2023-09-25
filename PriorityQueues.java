27b8825663f3e5202b7f351a774ae1ab7732a06b
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2023-09-25 16:59:54 +0530

    Priority Queues for Objects.

--- PriorityQueues.java
+++ PriorityQueues.java
@@ -2,19 +2,35 @@ import java.util.PriorityQueue;
 import java.util.Comparator;
 
 public class PriorityQueues {
+
+    static class Student implements Comparable<Student> {
+        String name;
+        int rank;
+
+        public Student(String name, int rank) {
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override
+        public int compareTo(Student s2) {
+            return this.rank - s2.rank;
+        }
+    }
+
     public static void main(String[] args) {
         /*This code will give output in Ascending order.
         If you want output in a Descending/Reverse order then add "Comparator.reverseOrder()" in PriorityQueue Constructor.*/
-        PriorityQueue<Integer> pq = new PriorityQueue<>(/*Comparator.reverseOrder()*/);
+        PriorityQueue<Student> pq = new PriorityQueue<>(/*Comparator.reverseOrder()*/);
 
         //Adding every element's in PQ takes O(log n) time. Where, 'N' is a number of elements already exist in PQ.
-        pq.add(3);
-        pq.add(4);
-        pq.add(1);
-        pq.add(7);
+        pq.add(new Student("A", 4));
+        pq.add(new Student("B", 5));
+        pq.add(new Student("C", 3));
+        pq.add(new Student("D", 6));
 
         while (!pq.isEmpty()) {
-            System.out.println(pq.peek()); //O(1)
+            System.out.println(pq.peek().name + " -> " + pq.peek().rank); //O(1)
             pq.remove(); //O(log n)
         }
     }
